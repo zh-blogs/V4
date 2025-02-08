b90755b8c138456c035cf398b721e53a2669c838
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-08 20:23:14 +0800

    feat: add blog page

---
+++ src/pages/blog/[...slug].vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="blog" class="prose max-w-none">
+    <ContentRenderer :value="blog" />
+  </div>
+  <div v-else>Blog Not Found</div>
+</template>
+
+<script lang="ts" setup>
+const COLLECTION_NAME = 'blog'
+const route = useRoute()
+const slug = route.params.slug
+const path = computed(() => {
+  if (Array.isArray(slug)) {
+    return `/${COLLECTION_NAME}/` + slug.join("/")
+  }
+  return `/${COLLECTION_NAME}/` + slug
+})
+const { data: blog } = await useAsyncData(`${path.value}`, () => {
+  return queryCollection(COLLECTION_NAME).path(path.value).first();
+})
+</script>
