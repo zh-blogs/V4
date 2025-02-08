3529d6b2ebf9b2c1a16301508944d5ac3d5ecbd8
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-08 20:22:47 +0800

    chore: add dependencies, update tailwindcss and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  srcDir: 'src/',
+  css: ['~/assets/css/main.css', 'remixicon/fonts/remixicon.css'],
   app: {
     head: {
-      title:"中文博客列表导航",
+      htmlAttrs: {
+        lang: 'zh'
+      },
+      title: "中文博客列表导航",
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.png' }
       ]
     }
+  },
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3,
+          searchDepth: 3
+        },
+        highlight: {
+          theme: {
+            default: 'github-light',
+            dark: 'github-dark',
+          },
+          langs: ['js', 'jsx', 'json', 'ts', 'tsx', 'vue', 'css', 'html', 'vue', 'bash', 'md', 'mdc', 'yaml', 'java']
+        }
+      }
+    },
   },
   postcss: {
     plugins: {
@@ -17,14 +39,15 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  content: {
-    build: {
-      markdown: {
-        toc: {
-          depth: 3
-        }
-      }
-    }
-  },
-  modules: ['@nuxt/content', '@nuxt/icon']
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    storage: 'localStorage',
+    storageKey: 'color-mode',
+    classPrefix: '',
+    classSuffix: ''
+  }
 })
