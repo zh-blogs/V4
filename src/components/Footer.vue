4ff8f4f52705208b3b9b6ef21c9ec22ea4b6fc9c
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-08 20:36:37 +0800

    fix: update footer to display current year dynamically

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,16 @@
     <div class="max-w-6xl mx-auto">
       <div class="flex flex-col justify-center text-neutral-500 dark:text-neutral-500 text-lg leading-10">
         <div class="w-full text-center"><span>尝试链接几乎所有的中文博客</span></div>
-        <div class="w-full text-center"><span>© 中文博客导航 2022 - 2025</span></div>
-        <div class="w-full text-center"><a href="https://github.com/zh-blogs"><i class="ri-github-fill"></i><span>zh-blogs/blog-daohang</span></a></div>
+        <div class="w-full text-center"><span>© 中文博客导航 2022 - {{ year }}</span></div>
+        <div class="w-full text-center"><a href="https://github.com/zh-blogs"><i
+              class="ri-github-fill"></i><span>zh-blogs/blog-daohang</span></a></div>
       </div>
     </div>
   </footer>
 </template>
 
 <script lang="ts" setup>
+const year = computed(() => {
+  return new Date().toLocaleDateString().split("/")[0]
+})
 </script>
