06771449a7583f8b81a165922253f345962d106d
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-08 20:18:34 +0800

    feat: update content configuration to customize blog field

--- content.config.ts
+++ content.config.ts
@@ -1,18 +1,34 @@
-import { defineContentConfig, defineCollection } from "@nuxt/content";
+import { defineContentConfig, defineCollection, z } from "@nuxt/content";
 
 export default defineContentConfig({
-  collections:{
+  collections: {
     page: defineCollection({
       type: 'page',
-      source:'/*.md',
+      source: '/*.md',
+      schema: z.object({
+        create: z.date(),
+        update: z.date(),
+        editor: z.array(z.string())
+      })
     }),
     blog: defineCollection({
-      type:'page',
-      source:'blog/*.md'
+      type: 'page',
+      source: 'blog/**/*.md',
+      schema: z.object({
+        top: z.boolean(),
+        date: z.date(),
+        tags: z.array(z.string()),
+        category: z.string()
+      })
     }),
-    docs:defineCollection({
-      type:'page',
-      source:"docs/*.md"
+    docs: defineCollection({
+      type: 'page',
+      source: "docs/**/*.md",
+      schema: z.object({
+        create: z.date(),
+        update: z.date(),
+        editor: z.array(z.string())
+      })
     })
   }
 })
