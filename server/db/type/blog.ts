b54ed5bb4baa82d34b016fc9c780e10d110d4ddc
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-12 22:03:50 +0800

    feat: add database configuration and schema for blog management

---
+++ server/db/type/blog.ts
@@ -0,0 +1,38 @@
+export interface Blog {
+  id: number;
+  idx: string;
+  name: string;
+  url: string;
+  sign: string;
+  main_tag: string;
+  sub_tag: string[];
+  feed: string[];
+  sitemap: string;
+  arch: string;
+  link_page: string;
+  join_time: Date;
+  update_time: Date;
+  from: string[];
+  status: string;
+  passed: boolean;
+  recommen: boolean;
+}
+
+export type WebSubmit = Omit<Blog, 'id' | 'idx' | 'join_time' | 'update_time' | 'status' | 'passed' | 'recommen'> & {
+  name: string;
+  url: string;
+  sign: string;
+  main_tag: string;
+  from: string[];
+  sub_tag?: string[];
+  feed?: any;
+  sitemap?: string;
+  arch?: string;
+  link_page?: string;
+};
+
+export type WebUpdate = Partial<WebSubmit>
+
+export type BotSubmit = Omit<Blog, 'id' | 'idx' | 'join_time' | 'update_time' | 'arch' | 'link_page' | 'status'>
+
+export type BotUpdate = Partial<BotSubmit>
