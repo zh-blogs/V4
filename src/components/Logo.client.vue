efd00d49e6350781c5e1db68b1b11b1c841c35f9
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-15 22:09:59 +0800

    feat: update header component and encapsulate icon components

---
+++ src/components/Logo.client.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="logoPath" alt="logo" />
+</template>
+
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const logoPath = computed(() => {
+  if (colorMode.preference === "system") {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches === true ? "/logo-dark.webp" : "/logo-light.webp"
+  }
+  return colorMode.preference === "dark" ? "/logo-dark.webp" : "/logo-light.webp"
+})
+</script>
