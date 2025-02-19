b7718f16a06661912274406e4b7a1f1ad799561c
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-19 22:45:58 +0800

    feat: add GitHub link

--- src/components/Footer.client.vue
+++ src/components/Footer.client.vue
@@ -3,7 +3,11 @@
     <div class="max-w-7xl mx-auto">
       <div class="flex flex-col justify-center text-neutral-500 dark:text-neutral-500 text-lg leading-10">
         <div class="w-full text-center"><span>尝试链接几乎所有的中文博客</span></div>
-        <div class="w-full text-center"><span>Copyright © 2022 - {{ year }} | 中文博客列表导航项目</span></div>
+        <div class="w-full text-center"><span>Copyright © 2022 - {{ year }} |
+            <NuxtLink to="https://github.com/zh-blogs">
+              <i class="ri-github-fill"></i> 中文博客列表导航项目
+            </NuxtLink>
+          </span></div>
       </div>
     </div>
   </footer>
