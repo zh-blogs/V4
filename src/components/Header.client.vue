27230499529d502aee2c7865f899869da5df636e
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-23 21:45:58 +0800

    feat: refactor the header component to incorporate daisyui

--- src/components/Header.client.vue
+++ src/components/Header.client.vue
@@ -1,100 +1,94 @@
 <template>
-  <header class="bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-200 mb-12 h-14 w-full shadow-xl relative">
-    <div class="h-full text-base font-bold max-w-7xl mx-auto flex flex-row items-center justify-between">
-      <div class="h-full w-auto">
+  <header class="bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-200 h-14 w-full mb-12 shadow-xl">
+    <div class="navbar max-w-7xl mx-auto h-full">
+      <div class="flex-none flex h-full">
         <NuxtLink to="/">
           <ElementsLogo class="h-full w-full object-contain" />
         </NuxtLink>
       </div>
-      <div class="h-full w-auto flex flex-1 items-center">
-        <div class="h-10 w-full flex">
-          <button type="button" title="查看博客列表"
-            class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-            <NuxtLink to="/list">博客列表</NuxtLink>
+      <div class="flex-1 flex h-full">
+        <div class="h-10 w-full flex self-end">
+          <button class="btn btn-ghost btn-sm font-medium">
+            <NuxtLink to="/list" class="text-base ">博客列表</NuxtLink>
           </button>
-          <button type="button" title="查看数据统计"
-            class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-            <NuxtLink to="/chart">数据统计</NuxtLink>
+          <button class="btn btn-ghost btn-sm font-medium">
+            <NuxtLink to="/charts" class="text-base ">数据统计</NuxtLink>
           </button>
-          <button type="button" title="查看项目文档"
-            class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-            <NuxtLink to="/docs">项目文档</NuxtLink>
+          <button class="btn btn-ghost btn-sm font-medium">
+            <NuxtLink to="/docs" class="text-base ">项目文档</NuxtLink>
           </button>
-          <button type="button" title="查看项目博客"
-            class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-            <NuxtLink to="/blog">项目博客</NuxtLink>
+          <button class="btn btn-ghost btn-sm font-medium">
+            <NuxtLink to="/blog" class="text-base ">项目博客</NuxtLink>
           </button>
-          <button type="button" title="关于我们"
-            class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-            <NuxtLink to="/about">关于我们</NuxtLink>
+          <button class="btn btn-ghost btn-sm font-medium">
+            <NuxtLink to="/about" class="text-base ">关于我们</NuxtLink>
           </button>
         </div>
       </div>
-      <div class="h-full mx-3 flex items-center">
-        <div class="h-10 w-full flex">
-          <button type="button" title="添加新博客"
-            class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-            <i class="ri-add-line mr-1"></i>
-            博客加入
+      <div class="flex-none flex h-full">
+        <div class="h-10 w-full flex self-end">
+          <button class="btn btn-ghost btn-sm font-medium">
+            <NuxtLink to="/about" class="text-base ">
+              <i class="ri-add-line"></i>
+              博客加入
+            </NuxtLink>
           </button>
-          <div class="relative group">
-            <button type="button" title="查看反馈选项"
-              class="h-full w-auto px-3 rounded-lg hover:bg-neutral-300 hover:dark:bg-neutral-700 flex items-center">
-              <i class="ri-feedback-line mr-1"></i>
+          <div class="dropdown dropdown-hover">
+            <button tabindex="0" role="button" class="btn btn-ghost btn-sm font-medium !text-base">
+              <i class="ri-feedback-line"></i>
               项目反馈
-              <i class="ri-arrow-down-s-line ml-1"></i>
+              <i class="ri-arrow-down-s-line"></i>
             </button>
-            <div
-              class="absolute hidden group-hover:block top-full right-0 whitespace-nowrap bg-white dark:bg-neutral-800 rounded-lg shadow-lg border dark:border-neutral-700">
-              <NuxtLink to="https://github.com/zh-blogs/blog-daohang/issues/new?template=03-Blog+Information+Report.yml"
-                target="_blank" rel="noopener noreferrer"
-                class="flex w-full items-center px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700 rounded-t-lg">
-                <i class="ri-error-warning-line mr-1"></i>
-                博客问题反馈
-                <i class="ri-external-link-line ml-1 text-xs text-neutral-500 self-end"></i>
-              </NuxtLink>
-              <NuxtLink
-                to="https://github.com/zh-blogs/blog-daohang/issues/new?template=04-Modify+%26+Improve+Blog+Information+Report.yml"
-                target="_blank" rel="noopener noreferrer"
-                class="flex w-full items-center px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700">
-                <i class="ri-edit-line mr-1"></i>
-                修改博客信息
-                <i class="ri-external-link-line ml-1 text-xs text-neutral-500 self-end"></i>
-              </NuxtLink>
-              <NuxtLink
-                to="https://github.com/zh-blogs/blog-daohang/issues/new?template=05-Selected+Blog+Application.yml"
-                target="_blank" rel="noopener noreferrer"
-                class="flex w-full items-center px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700 rounded-b-lg">
-                <i class="ri-hand mr-1"></i>
-                精选博客推荐
-                <i class="ri-external-link-line ml-1 text-xs text-neutral-500 self-end"></i>
-              </NuxtLink>
-            </div>
+            <ul tabindex="0" class="dropdown-content menu bg-neutral-100 dark:bg-neutral-800 rounded-box z-1 shadow-sm">
+              <li class="">
+                <NuxtLink class="whitespace-nowrap"
+                  to="https://github.com/zh-blogs/blog-daohang/issues/new?template=03-Blog+Information+Report.yml"
+                  target="_blank" rel="noopener noreferrer">
+                  <i class="ri-error-warning-line"></i>
+                  博客问题反馈
+                  <i class="ri-external-link-line text-xs text-neutral-500 self-end"></i>
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink class="whitespace-nowrap"
+                  to="https://github.com/zh-blogs/blog-daohang/issues/new?template=04-Modify+%26+Improve+Blog+Information+Report.yml"
+                  target="_blank" rel="noopener noreferrer">
+                  <i class="ri-edit-line"></i>
+                  修改博客信息
+                  <i class="ri-external-link-line text-xs text-neutral-500 self-end"></i>
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink class="whitespace-nowrap"
+                  to="https://github.com/zh-blogs/blog-daohang/issues/new?template=05-Selected+Blog+Application.yml"
+                  target="_blank" rel="noopener noreferrer">
+                  <i class="ri-hand mr-1"></i>
+                  精选博客推荐
+                  <i class="ri-external-link-line ml-1 text-xs text-neutral-500 self-end"></i>
+                </NuxtLink>
+              </li>
+            </ul>
           </div>
         </div>
         <div
-          class="flex flex-row items-center justify-center before:content-[''] before:h-6 before:w-px before:bg-neutral-500 before:mx-3">
-          <button type="button" title="切换至浅色模式" @click="colorMode.preference = 'light'"
-            class="p-1 rounded-lg hover:bg-neutral-300 dark:hover:bg-neutral-700"
+          class="flex flex-row items-center justify-center before:content-[''] before:h-6 before:w-px before:bg-neutral-500 before:mx-2">
+          <button @click="colorMode.preference = 'light'" class="btn btn-square btn-xs btn-ghost p-3"
             :class="{ 'text-blue-500': colorMode.preference === 'light' }">
-            <i class="ri-sun-line py-1 px-0.5"></i>
+            <i class="ri-sun-line text-base"></i>
           </button>
-          <button type="button" title="切换至深色模式" @click="colorMode.preference = 'dark'"
-            class="p-1 rounded-lg hover:bg-neutral-300 dark:hover:bg-neutral-700"
+          <button @click="colorMode.preference = 'dark'" class="btn btn-square btn-xs btn-ghost p-3"
             :class="{ 'text-blue-500': colorMode.preference === 'dark' }">
-            <i class="ri-moon-line py-1 px-0.5"></i>
+            <i class="ri-moon-line text-base"></i>
           </button>
-          <button type="button" title="跟随系统主题" @click="colorMode.preference = 'system'"
-            class="p-1 rounded-lg hover:bg-neutral-300 dark:hover:bg-neutral-700"
+          <button @click="colorMode.preference = 'system'" class="btn btn-square btn-xs btn-ghost p-3"
             :class="{ 'text-blue-500': colorMode.preference === 'system' }">
-            <i class="ri-computer-line py-1 px-0.5"></i>
+            <i class="ri-computer-line text-base"></i>
           </button>
         </div>
         <div v-if="!session.user"
-          class="w-auto flex flex-row items-center justify-center before:content-[''] before:h-6 before:w-px before:bg-neutral-500 before:mx-3">
-          <button type="button" title="GitHub 登录" @click="openInPopup('/auth/github')"
-            class="p-1 rounded-lg hover:bg-neutral-300 dark:hover:bg-neutral-700">
-            <i class="ri-admin-line"></i>
+          class="flex flex-row items-center justify-center before:content-[''] before:h-6 before:w-px before:bg-neutral-500 before:mx-2">
+          <button @click="openInPopup('/auth/github')" class="btn btn-square btn-xs btn-ghost p-3">
+            <i class="ri-admin-line text-base"></i>
           </button>
         </div>
         <div v-if="session.user"
