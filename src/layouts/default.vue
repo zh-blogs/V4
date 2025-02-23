d1fd9b1b4c6de4d764b5718c09a937c6a5af6f4a
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-23 21:14:43 +0800

    feat: replace default layout with home layout and update styles for daisyui integration

--- src/layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Header />
-  <div class="bg-neutral-100 dark:bg-neutral-900 dark:shadow-none shadow-xl max-w-7xl mx-auto h-full p-10">
-    <slot />
-  </div>
-  <Footer />
-</template>
---
+++ src/layouts/home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Header />
+  <div class="max-w-7xl bg-neutral-100 dark:bg-neutral-800 mx-auto">
+    <slot />
+  </div>
+  <Footer />
+</template>
