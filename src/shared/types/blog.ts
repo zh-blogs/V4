e8c8fdd4c986c0dfcc4bbceae53d9008726c1e94
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-02-26 23:37:47 +0800

    feat: utilize the Zod library to define the types for WebSubmit and BotUpdate

--- src/shared/types/blog.ts
+++ src/shared/types/blog.ts
@@ -1,39 +1,39 @@
-export interface Blog {
-  id: number;
-  idx: string;
-  name: string;
-  url: string;
-  sign: string;
-  main_tag: string;
-  sub_tag: string[];
-  feed: string[];
-  sitemap: string;
-  arch: string;
-  link_page: string;
-  join_time: Date;
-  update_time: Date;
-  from: string[];
-  status: string;
-  passed: boolean;
-  recommen: boolean;
-  saveweb_id: string;
-}
-export type WebSubmit = Omit<Blog, 'id' | 'idx' | 'join_time' | 'update_time' | 'status' | 'passed' | 'recommen'> & {
-  name: string;
-  url: string;
-  sign: string;
-  main_tag: string;
-  from: string[];
-  sub_tag?: string[];
-  feed?: any;
-  sitemap?: string;
-  arch?: string;
-  link_page?: string;
-  saveweb_id?: string;
-};
+import { createInsertSchema } from 'drizzle-zod';
+import { z } from "zod";
+import { blogs } from "~~/server/db/schema";
 
-export type WebUpdate = Partial<WebSubmit>
+export const WebSubmit = z.object({
+  name: z.string(),
+  url: z.string().url(),
+  sign: z.string(),
+  main_tag: z.string(),
+  sub_tag: z.array(z.string()).optional(),
+  feed: z.array(z.string()).optional(),
+  sitemap: z.string().optional(),
+  arch: z.string().optional(),
+  link_page: z.string().optional(),
+  saveweb_id: z.string().optional(),
+  from: z.array(z.string()).optional(),
+})
 
-export type BotSubmit = Omit<Blog, 'id' | 'idx' | 'join_time' | 'update_time' | 'arch' | 'link_page' | 'status'>
+export type WebSubmit = z.infer<typeof WebSubmit>
 
-export type BotUpdate = Partial<BotSubmit>
+export const BotUpdate = z.object({
+  blogs: z.array(z.object({
+    name: z.string(),
+    url: z.string().url(),
+    sign: z.string().optional(),
+    main_tag: z.string().optional(),
+    sub_tag: z.array(z.string()).optional(),
+    feed: z.array(z.string()).optional(),
+    sitemap: z.string().optional(),
+    arch: z.string().optional(),
+    from: z.array(z.string()),
+    status: z.string(),
+    passed: z.boolean(),
+    recommen: z.boolean().optional(),
+    saveweb_id: z.string().optional(),
+  }))
+})
+
+export type BotUpdate = z.infer<typeof BotUpdate>
