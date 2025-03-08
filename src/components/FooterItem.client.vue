a7010bc7b38a0f8463940fd733f68cd06c0deca6
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-08 23:51:24 +0800

    refactor: rewrite header and footer with mobile responsiveness

--- src/components/FooterItem.client.vue
+++ src/components/FooterItem.client.vue
@@ -1,12 +1,12 @@
 <template>
   <footer
-    class="footer sm:footer-horizontal text-base-content mx-auto max-w-7xl px-5 py-10 md:p-10"
+    class="footer sm:footer-horizontal text-base-content mx-auto max-w-7xl justify-evenly px-5 py-10 md:p-10"
   >
     <aside class="mx-auto flex flex-col">
       <div class="flex flex-row items-center gap-2 md:gap-5">
         <ElementsLogo class="h-24 w-24 object-contain md:h-28 md:w-28" />
         <div
-          class="leading-7 font-bold text-neutral-400 md:text-base dark:text-neutral-500"
+          class="text-sm leading-7 font-bold text-neutral-400 md:text-base dark:text-neutral-500"
         >
           <p>尝试链接几乎所有的中文博客</p>
           <NuxtLink
@@ -30,80 +30,70 @@
       </div>
     </aside>
     <div class="mx-auto grid grid-cols-2">
-      <nav class="grid grid-cols-1 leading-7">
+      <nav class="grid grid-cols-1 pl-6 leading-7 md:pl-0">
         <h6 class="footer-title">快速访问</h6>
-        <NuxtLink
-          to="/list"
-          class="link link-hover"
+        <template
+          v-for="item in quickLinks"
+          :key="item.path"
         >
-          博客列表
-        </NuxtLink>
-        <NuxtLink
-          to="/charts"
-          class="link link-hover"
-        >
-          数据统计
-        </NuxtLink>
-        <NuxtLink
-          to="/random"
-          class="link link-hover"
-        >
-          随机跳转
-        </NuxtLink>
-        <NuxtLink
-          to="/docs"
-          class="link link-hover"
-        >
-          项目文档
-        </NuxtLink>
-        <NuxtLink
-          to="/blog"
-          class="link link-hover"
-        >
-          项目博客
-        </NuxtLink>
-        <NuxtLink
-          to="/about"
-          class="link link-hover"
-        >
-          关于我们
-        </NuxtLink>
+          <NuxtLink
+            :to="item.path"
+            class="link link-hover"
+          >
+            {{ item.name }}
+          </NuxtLink>
+        </template>
       </nav>
       <nav class="grid grid-cols-1 leading-7">
         <h6 class="footer-title">友情链接</h6>
-        <NuxtLink
-          to="https://www.travellings.cn/"
-          class="link link-hover"
+        <template
+          v-for="item in friendLinks"
+          :key="item.path"
         >
-          开往
-        </NuxtLink>
-        <NuxtLink
-          to="https://www.boyouquan.com/"
-          class="link link-hover"
-        >
-          博友圈
-        </NuxtLink>
-        <NuxtLink
-          to="https://bf.zzxworld.com/"
-          class="link link-hover"
-        >
-          BlogFinder
-        </NuxtLink>
-        <NuxtLink
-          to="https://github.com/timqian/chinese-independent-blogs"
-          class="link link-hover"
-        >
-          中文独立博客列表
-        </NuxtLink>
-        <NuxtLink
-          to="http://www.jetli.com.cn/"
-          class="link link-hover"
-        >
-          优秀个人独立博客导航
-        </NuxtLink>
+          <NuxtLink
+            :to="item.path"
+            class="link link-hover"
+          >
+            {{ item.name }}
+          </NuxtLink>
+        </template>
       </nav>
     </div>
   </footer>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { LinkItems } from '~/types/components'
+
+const quickLinks: LinkItems = [
+  { name: '博客列表', path: '/list' },
+  { name: '数据统计', path: '/charts' },
+  { name: '随机跳转', path: '/random' },
+  { name: '项目文档', path: '/docs' },
+  { name: '项目博客', path: '/blog' },
+  { name: '关于我们', path: '/about' },
+]
+
+const friendLinks: LinkItems = [
+  {
+    name: '开往',
+    path: 'https://www.travellings.cn/',
+  },
+  {
+    name: '博友圈',
+    path: 'https://www.boyouquan.com/',
+  },
+  {
+    name: 'BlogFinder',
+    path: 'https://bf.zzxworld.com/',
+  },
+  {
+    name: '中文独立博客列表',
+    path: 'https://github.com/timqian/chinese-independent-blogs',
+  },
+  {
+    name: '优秀个人独立博客导航',
+    path: 'http://www.jetli.com.cn/',
+  },
+]
+</script>
--- src/components/HeaderItem.client.vue
+++ src/components/HeaderItem.client.vue
@@ -1,185 +1,280 @@
 <template>
-  <header class="bg-base-200 mb-12 h-16 w-full shadow-xl">
-    <div class="navbar mx-auto h-full max-w-7xl p-0">
-      <div class="flex h-full flex-none">
+  <header class="bg-base-200 h-14 px-3 md:h-18">
+    <div class="drawer mx-auto flex h-full max-w-7xl justify-between">
+      <div class="h-full p-1">
         <NuxtLink to="/">
           <ElementsLogo class="h-full w-full object-contain" />
         </NuxtLink>
       </div>
-      <div class="flex h-full flex-1">
+      <div class="hidden h-full flex-1 md:flex">
         <div class="flex flex-row items-center justify-center">
-          <button class="btn btn-sm btn-ghost h-full font-medium">
-            <NuxtLink
-              to="/list"
-              class="text-base"
-              >博客列表</NuxtLink
-            >
-          </button>
-          <button class="btn btn-sm btn-ghost h-full font-medium">
-            <NuxtLink
-              to="/charts"
-              class="text-base"
-              >数据统计</NuxtLink
-            >
-          </button>
-          <button class="btn btn-sm btn-ghost h-full font-medium">
-            <NuxtLink
-              to="/docs"
-              class="text-base"
-              >项目文档</NuxtLink
-            >
-          </button>
-          <button class="btn btn-sm btn-ghost h-full font-medium">
-            <NuxtLink
-              to="/blog"
-              class="text-base"
-              >项目博客</NuxtLink
-            >
-          </button>
-          <button class="btn btn-sm btn-ghost h-full font-medium">
-            <NuxtLink
-              to="/about"
-              class="text-base"
-              >关于我们</NuxtLink
-            >
-          </button>
+          <template
+            v-for="item in leftMenuItems"
+            :key="item.path"
+          >
+            <button class="btn btn-sm btn-ghost h-full font-medium">
+              <NuxtLink
+                :to="item.path"
+                class="text-base"
+                >{{ item.name }}</NuxtLink
+              >
+            </button>
+          </template>
         </div>
       </div>
-      <div class="flex h-full flex-none">
-        <div class="flex flex-row items-center justify-center">
-          <button class="btn btn-sm btn-ghost h-full font-medium">
-            <NuxtLink
-              to="/about"
-              class="text-base"
-            >
-              <i class="ri-add-line" />
-              提交博客
-            </NuxtLink>
+      <div class="flex flex-row items-center justify-center">
+        <button class="btn btn-sm btn-ghost h-full font-medium">
+          <NuxtLink
+            to="/about"
+            class="text-base"
+          >
+            <i class="ri-add-line" />
+            提交博客
+          </NuxtLink>
+        </button>
+        <div class="dropdown-hover dropdown h-full">
+          <button
+            tabindex="0"
+            role="button"
+            class="btn btn-sm btn-ghost !h-full !text-base font-medium"
+          >
+            <i class="ri-feedback-line" />
+            项目反馈
+            <i class="ri-arrow-down-s-line" />
           </button>
-          <div class="dropdown-hover dropdown h-full">
-            <button
-              tabindex="0"
-              role="button"
-              class="btn btn-sm btn-ghost !h-full !text-base font-medium"
+          <ul
+            tabindex="0"
+            class="dropdown-content menu rounded-box z-1 bg-neutral-100 shadow-sm dark:bg-neutral-800"
+          >
+            <template
+              v-for="item in rightMenuItems"
+              :key="item.path"
             >
-              <i class="ri-feedback-line" />
-              项目反馈
-              <i class="ri-arrow-down-s-line" />
-            </button>
-            <ul
-              tabindex="0"
-              class="dropdown-content menu rounded-box z-1 bg-neutral-100 shadow-sm dark:bg-neutral-800"
-            >
-              <li class="">
+              <li>
                 <NuxtLink
                   class="whitespace-nowrap"
-                  to="https://github.com/zh-blogs/blog-daohang/issues/new?template=03-Blog+Information+Report.yml"
+                  :to="item.path"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <i class="ri-error-warning-line" />
-                  博客问题反馈
+                  {{ item.name }}
                   <i
                     class="ri-external-link-line self-end text-xs text-neutral-500"
                   />
                 </NuxtLink>
               </li>
-              <li>
-                <NuxtLink
-                  class="whitespace-nowrap"
-                  to="https://github.com/zh-blogs/blog-daohang/issues/new?template=04-Modify+%26+Improve+Blog+Information+Report.yml"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i class="ri-edit-line" />
-                  修改博客信息
-                  <i
-                    class="ri-external-link-line self-end text-xs text-neutral-500"
-                  />
-                </NuxtLink>
-              </li>
-              <li>
-                <NuxtLink
-                  class="whitespace-nowrap"
-                  to="https://github.com/zh-blogs/blog-daohang/issues/new?template=05-Selected+Blog+Application.yml"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i class="ri-hand" />
-                  精选博客推荐
-                  <i
-                    class="ri-external-link-line ml-1 self-end text-xs text-neutral-500"
-                  />
-                </NuxtLink>
-              </li>
-            </ul>
-          </div>
+            </template>
+          </ul>
         </div>
-        <div
-          class="flex flex-row items-center justify-center before:mx-2 before:h-6 before:w-px before:bg-neutral-500 before:content-['']"
-        >
+      </div>
+      <div class="flex flex-row items-center">
+        <div>
           <button
-            class="btn btn-square btn-xs btn-ghost p-3"
+            class="btn btn-square btn-sm btn-ghost"
             :class="{ 'text-blue-500': colorMode.preference === 'light' }"
             @click="colorMode.preference = 'light'"
           >
             <i class="ri-sun-line text-base" />
           </button>
           <button
-            class="btn btn-square btn-xs btn-ghost p-3"
+            class="btn btn-square btn-sm btn-ghost"
             :class="{ 'text-blue-500': colorMode.preference === 'dark' }"
             @click="colorMode.preference = 'dark'"
           >
             <i class="ri-moon-line text-base" />
           </button>
           <button
-            class="btn btn-square btn-xs btn-ghost p-3"
+            class="btn btn-square btn-sm btn-ghost"
             :class="{ 'text-blue-500': colorMode.preference === 'system' }"
             @click="colorMode.preference = 'system'"
           >
             <i class="ri-computer-line text-base" />
           </button>
         </div>
+        <input
+          id="menu-sidebar"
+          type="checkbox"
+          class="drawer-toggle"
+        />
         <div
-          v-if="!session.user"
-          class="flex flex-row items-center justify-center before:mx-2 before:h-6 before:w-px before:bg-neutral-500 before:content-['']"
+          class="drawer-content flex items-center before:mx-2 before:h-6 before:w-px before:bg-neutral-500 before:content-[''] md:hidden"
         >
-          <button
-            class="btn btn-square btn-xs btn-ghost p-3"
-            @click="openInPopup('/auth/github')"
+          <label
+            for="menu-sidebar"
+            class="btn btn-square btn-sm btn-ghost"
           >
-            <i class="ri-admin-line text-base" />
-          </button>
+            <i class="ri-menu-line text-base"></i>
+          </label>
         </div>
-        <div
-          v-if="session.user"
-          class="group relative flex w-auto flex-row items-center justify-center text-center text-sm font-normal whitespace-nowrap before:mx-3 before:h-6 before:w-px before:bg-neutral-500 before:content-['']"
-        >
-          <div
-            class="rounded-lg px-3 py-1 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+
+        <div class="drawer-side z-10 md:hidden">
+          <label
+            for="menu-sidebar"
+            aria-label="close sidebar"
+            class="drawer-overlay"
+          ></label>
+          <ul
+            class="menu bg-base-200 text-base-content z-10 min-h-full w-60 pt-3"
           >
-            <p>欢迎你，管理员</p>
-            <p>
-              {{ session.user.name }}<i class="ri-arrow-down-s-line ml-1" />
-            </p>
+            <li @click="closeDrawer">
+              <header>
+                <NuxtLink
+                  to="/"
+                  class="flex h-18 w-full flex-row items-center justify-start"
+                >
+                  <ElementsLogo class="h-full object-contain" />
+                  <div>
+                    <h1 class="text-xl font-bold">集博栈</h1>
+                    <p class="text-sm opacity-50">原中文博客列表导航</p>
+                  </div>
+                </NuxtLink>
+              </header>
+            </li>
+            <li>
+              <details open>
+                <summary class="text-sm opacity-70">站内导航</summary>
+                <ul>
+                  <template
+                    v-for="item in leftMenuItems"
+                    :key="item.path"
+                  >
+                    <li @click="closeDrawer">
+                      <NuxtLink :to="item.path">{{ item.name }}</NuxtLink>
+                    </li>
+                  </template>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary class="text-sm opacity-70">项目反馈</summary>
+                <ul>
+                  <template
+                    v-for="item in rightMenuItems"
+                    :key="item.path"
+                  >
+                    <li
+                      class="flex flex-row"
+                      @click="closeDrawer"
+                    >
+                      <NuxtLink
+                        :to="item.path"
+                        class="pr-1"
+                        >{{ item.name }}</NuxtLink
+                      >
+                      <i
+                        class="ri-external-link-line self-end p-0 py-1.5 text-xs text-neutral-500"
+                      />
+                    </li>
+                  </template>
+                </ul>
+              </details>
+            </li>
+            <div class="divider"></div>
+            <li
+              class="px-3"
+              @click="closeDrawer"
+            >
+              <button class="btn btn-sm btn-outline">
+                <NuxtLink
+                  to=""
+                  class="text-base"
+                >
+                  <i class="ri-add-line" />
+                  提交博客
+                </NuxtLink>
+              </button>
+            </li>
+            <div class="divider"></div>
+            <li class="px-3 opacity-40">
+              <div
+                v-if="!session.user"
+                class="flex h-10 w-full items-center justify-center p-0"
+              >
+                <button
+                  class="flex h-full w-full items-center justify-center gap-2"
+                  @click="openInPopup('/auth/github')"
+                >
+                  <i class="ri-admin-line text-base" />
+                  管理员您好，请先登录
+                </button>
+              </div>
+              <details
+                v-if="session.user"
+                open
+              >
+                <summary class="text-sm">
+                  欢迎你，管理员
+                  <br />
+                  {{ session.user.name }}
+                </summary>
+                <ul>
+                  <li @click="closeDrawer">
+                    <NuxtLink
+                      to="/admin"
+                      class="flex w-full items-center rounded-t-lg px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+                    >
+                      <i class="ri-speed-up-line mr-1" />
+                      进入控制台
+                    </NuxtLink>
+                  </li>
+                  <li @click="closeDrawer">
+                    <button
+                      class="flex w-full items-center rounded-b-lg px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+                      @click="handleLogout"
+                    >
+                      <i class="ri-logout-box-r-line mr-1" />
+                      退出登录
+                    </button>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+        <div class="hidden md:block">
+          <div
+            v-if="!session.user"
+            class="flex flex-row items-center justify-center before:mx-1 before:h-6 before:w-px before:bg-neutral-500 before:content-['']"
+          >
+            <button
+              class="btn btn-square btn-xs btn-ghost p-3"
+              @click="openInPopup('/auth/github')"
+            >
+              <i class="ri-admin-line text-base" />
+            </button>
           </div>
           <div
-            class="absolute top-full right-0 hidden rounded-lg border bg-white whitespace-nowrap shadow-lg group-hover:block dark:border-neutral-700 dark:bg-neutral-800"
+            v-if="session.user"
+            class="group relative flex w-auto flex-row items-center justify-center text-center text-sm font-normal whitespace-nowrap before:mx-1 before:h-6 before:w-px before:bg-neutral-500 before:content-['']"
           >
-            <NuxtLink
-              to="/admin"
-              class="flex w-full items-center rounded-t-lg px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+            <div
+              class="rounded-lg px-2 py-1 text-end hover:bg-neutral-300 dark:hover:bg-neutral-700"
             >
-              <i class="ri-speed-up-line mr-1" />
-              进入控制台
-            </NuxtLink>
-            <button
-              class="flex w-full items-center rounded-b-lg px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700"
-              @click="handleLogout"
+              <p>欢迎你，管理员</p>
+              <p>
+                {{ session.user.name }}<i class="ri-arrow-down-s-line ml-1" />
+              </p>
+            </div>
+            <div
+              class="absolute top-full right-0 hidden rounded-lg border bg-white whitespace-nowrap shadow-lg group-hover:block dark:border-neutral-700 dark:bg-neutral-800"
             >
-              <i class="ri-logout-box-r-line mr-1" />
-              退出登录
-            </button>
+              <NuxtLink
+                to="/admin"
+                class="flex w-full items-center rounded-t-lg px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+              >
+                <i class="ri-speed-up-line mr-1" />
+                进入控制台
+              </NuxtLink>
+              <button
+                class="flex w-full items-center rounded-b-lg px-4 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+                @click="handleLogout"
+              >
+                <i class="ri-logout-box-r-line mr-1" />
+                退出登录
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -188,14 +283,59 @@
 </template>
 
 <script lang="ts" setup>
-const colorMode = useColorMode()
+import type { LinkItems } from '~/types/components'
+
 const { openInPopup, clear, session } = useUserSession()
+const colorMode = useColorMode()
+const leftMenuItems: LinkItems = [
+  {
+    name: '博客列表',
+    path: '/list',
+  },
+  {
+    name: '数据统计',
+    path: '/charts',
+  },
+  {
+    name: '项目文档',
+    path: '/docs',
+  },
+  {
+    name: '项目博客',
+    path: '/blog',
+  },
+  {
+    name: '关于我们',
+    path: '/about',
+  },
+]
+const rightMenuItems: LinkItems = [
+  {
+    name: '博客问题反馈',
+    path: 'https://github.com/zh-blogs/blog-daohang/issues/new?template=03-Blog+Information+Report.yml',
+  },
+  {
+    name: '修改博客信息',
+    path: 'https://github.com/zh-blogs/blog-daohang/issues/new?template=04-Modify+%26+Improve+Blog+Information+Report.yml',
+  },
+  {
+    name: '精选博客推荐',
+    path: 'https://github.com/zh-blogs/blog-daohang/issues/new?template=05-Selected+Blog+Application.yml',
+  },
+]
 
 const handleLogout = async () => {
   await clear()
   const route = useRoute()
   if (route.path.startsWith('/admin')) {
     await navigateTo('/')
+  }
+}
+
+const closeDrawer = () => {
+  const checkbox = document.getElementById('menu-sidebar') as HTMLInputElement
+  if (checkbox) {
+    checkbox.checked = false
   }
 }
 </script>
