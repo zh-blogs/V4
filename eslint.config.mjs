21786f1fe00c85900e7253ee09c0e68f4fad6577
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-13 15:26:21 +0800

    chore: improve collaboration tool configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default withNuxt(
       'unused-imports': unusedImports,
     },
     rules: {
-      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'no-unused-vars': 'off',
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
