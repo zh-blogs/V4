80be62d5cfff21b28e34867268cb2b89f768e3a3
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-13 09:41:20 +0800

    chore: add formatter configuration to clean up unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,25 @@
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import unusedImports from 'eslint-plugin-unused-imports'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({}, eslintPluginPrettierRecommended)
+export default withNuxt(
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
+)
