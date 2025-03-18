a632d557ce02662760ccacd8445f31ea63899124
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-18 22:36:38 +0800

    feat: adjust UI and add redirect notice from old site

--- src/app.vue
+++ src/app.vue
@@ -3,3 +3,23 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const { $useAlert } = useNuxtApp()
+
+if (import.meta.client) {
+  let referrerURL: URL | null = null
+  const referrer: string = window.document.referrer
+  const oldSiteURL: string = 'zhblogs.ohyee.cc'
+  if (referrer) {
+    referrerURL = new URL(referrer)
+    if (referrerURL.hostname === oldSiteURL) {
+      $useAlert.info({
+        title: '新站点启用通知',
+        message: `您似乎是从我们的旧网站 ${oldSiteURL} 重定向过来的，我们已经启用全新域名和站点程序，请及时更新您所记载或使用的本站的旧信息`,
+        duration: 10000,
+      })
+    }
+  }
+}
+</script>
