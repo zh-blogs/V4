a0ee9c4aff3151e2df6d2b1a802694cf8fd6bf52
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-20 17:57:08 +0800

    refactor: optimize Zod data validation logic

--- src/shared/types/blog.ts
+++ src/shared/types/blog.ts
@@ -22,23 +22,40 @@ export const BaseBlogSchema = z.object({
     .string({ message: '不正确的数据格式' })
     .url({ message: 'URL 格式不正确' })
     .max(64, { message: 'URL 长度不得超过 64 字符' }),
-  sign: z.string({ message: '不正确的数据格式' }).default(''),
+  sign: z.string({ message: '不正确的数据格式' }),
   main_tag: z.enum(MAIN_TAGS, {
     errorMap: () => ({ message: '无效的主标签类型' }),
   }),
-  sub_tag: z
-    .array(z.string({ message: '不正确的数据格式' }))
-    .max(10)
-    .default([]),
-  feed: z
-    .array(
-      z.string({ message: '不正确的数据格式' }),
-      // .url({ message: 'Feed URL 格式不正确' }),
+  sub_tag: z.nullable(
+    z
+      .array(
+        z
+          .string({ message: '不正确的数据格式' })
+          .min(1, { message: '空字符串是不能成为标签的哦' }),
+      )
+      .max(10),
+  ),
+  feed: z.nullable(
+    z.array(
+      z
+        .string({ message: '不正确的数据格式' })
+        .max(128, { message: 'Feed URL 长度不得超过 128 字符' })
+        .refine((arg) => {
+          if (!arg) {
+            return true
+          }
+          try {
+            new URL(arg)
+            return true
+          } catch {
+            return false
+          }
+        }),
       {
         errorMap: () => ({ message: '无效的 Feed URL 类型' }),
       },
-    )
-    .default([]),
+    ),
+  ),
   from: z
     .array(
       z.enum(FROM_SOURCES, {
@@ -48,33 +65,51 @@ export const BaseBlogSchema = z.object({
         errorMap: () => ({ message: '无效的数据来源类型' }),
       },
     )
-    .min(1, { message: '数据来源不能为空' })
-    .default(['WebSubmit']),
-  sitemap: z
-    .string({ message: '不正确的数据格式' })
-    // .url({ message: 'Sitemap URL 格式不正确' })
-    .max(128, { message: 'Sitemap URL 长度不得超过 128 字符' })
-    .default(''),
-  link_page: z
-    .string()
-    // .url({ message: '友链页面 URL 格式不正确' })
-    .max(128, { message: '友链页面 URL 长度不得超过 128 字符' })
-    .default(''),
+    .min(1, { message: '数据来源不能为空' }),
+  sitemap: z.nullable(
+    z
+      .string({ message: '不正确的数据格式' })
+      .max(128, { message: 'Sitemap URL 长度不得超过 128 字符' })
+      .refine((arg) => {
+        if (!arg) {
+          return true
+        }
+        try {
+          new URL(arg)
+          return true
+        } catch {
+          return false
+        }
+      }),
+  ),
+  link_page: z.nullable(
+    z
+      .string({ message: '不正确的数据格式' })
+      .max(128, { message: 'Sitemap URL 长度不得超过 128 字符' })
+      .refine((arg) => {
+        if (!arg) {
+          return true
+        }
+        try {
+          new URL(arg)
+          return true
+        } catch {
+          return false
+        }
+      }),
+  ),
   arch: z
     .string({ message: '不正确的数据格式' })
-    .max(32, { message: '博客架构名称不超过 16 字符' })
-    .default(''),
-  status: z
-    .enum(STATUS_TYPES, {
-      errorMap: () => ({ message: '无效的状态码类型' }),
-    })
-    .default('OK'),
-  passed: z.boolean().default(false),
-  recommen: z.boolean().default(false),
-  join_time: z.date().default(() => new Date()),
-  update_time: z.date().default(() => new Date()),
-  saveweb_id: z.string().default(''),
-  access_count: z.number().default(0),
+    .max(32, { message: '博客架构名称不超过 16 字符' }),
+  status: z.enum(STATUS_TYPES, {
+    errorMap: () => ({ message: '无效的状态码类型' }),
+  }),
+  passed: z.boolean(),
+  recommen: z.boolean(),
+  join_time: z.date(),
+  update_time: z.date(),
+  saveweb_id: z.string(),
+  access_count: z.number(),
 })
 
 export type BaseBlog = z.infer<typeof BaseBlogSchema>
