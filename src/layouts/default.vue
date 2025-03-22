157df75d9b63a8f9ad97f413215c7d7d7a177e49
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-23 05:09:13 +0800

    feat: update default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <HeaderItem class="fixed top-0 z-50 w-full" />
-    <div class="bg-base-200 mx-auto mt-36 w-full break-all lg:max-w-7xl">
+    <div
+      class="bg-base-200 mx-auto mt-28 w-full break-all sm:mt-36 lg:max-w-7xl"
+    >
       <slot />
     </div>
     <FooterItem />
