92eacc7401ed35bcb544da19538361dafde12987
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-03-23 05:11:17 +0800

    feat: add some empty files to prepare for the new feature

---
+++ src/layouts/content.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- TODO -->
+    <HeaderItem />
+    <div class="mx-auto grid h-96 max-w-7xl grid-cols-[220px_1fr] gap-5">
+      <SideBar />
+      <div class="">
+        <slot />
+      </div>
+    </div>
+    <FooterItem />
+  </div>
+</template>
