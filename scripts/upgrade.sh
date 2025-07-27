95b01ee0796e77fefca22b7e44592795a2ac2bbd
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-07-27 22:14:01 +0800

    feat: add script for zhblogs V4 project upgrade and deploy

---
+++ scripts/upgrade.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# deploy.sh - Automated upgrade script for the zhblogs V4 project.
+# Author: MYXXTS
+# Date: 2025-07-27
+# Description: This script automates the upgrade process for the zhblogs V4 project with github webhook.
+
+echo "======Starting deployment script...======"
+# Navigate to the project root directory
+cd "$(git rev-parse --show-toplevel)" || exit 1
+echo "Navigated to project root: $(pwd)"
+# Store the old commit hash
+OLD_COMMIT=$(git rev-parse HEAD)
+# Pull the latest changes from the main branch
+echo "Pulling latest changes from main branch..."
+git pull origin main || { echo "Failed to pull latest changes"; exit 1; }
+# Check if the commit hash has changed
+NEW_COMMIT=$(git rev-parse HEAD)
+# Check commit hash
+if [ "$OLD_COMMIT" = "$NEW_COMMIT" ]; then
+  echo "No new changes detected, exiting."
+  exit 0
+fi
+echo "New changes detected, proceeding with upgrade."
+# Check if there are any changes in the .env.example file
+git diff --name-only $OLD_COMMIT $NEW_COMMIT -- .env.example
+if [ $? -eq 0 ]; then
+  echo "No changes in .env.example, continuing with existing environment file."
+else
+  echo "Changes detected in .env.example, please update your .env file accordingly."
+  exit 92
+fi
+# Install dependencies
+echo "Installing dependencies..."
+pnpm -r install || { echo "Failed to install dependencies"; exit 1; }
+# Build the project
+echo "Building the project..."
+pnpm build || { echo "Build failed"; exit 1; }
+# Reload the project
+echo "Reload the project with PM2..."
+pm2 reload ./configs/ecosystem.config.js --only "zhblogs-server,zhblogs-web" || { echo "Failed to reload PM2"; exit 1; }
+
